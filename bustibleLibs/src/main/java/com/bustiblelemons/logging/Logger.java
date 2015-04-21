b54b461a6829803a83a8fb4ea264a825dd523826
Kuzniarski Jacek <jkuzniarski@gmail.com>
2015-04-21 18:33:51 +0200

    yeah, static methods in Logger class what?

--- bustibleLibs/src/main/java/com/bustiblelemons/logging/Logger.java
+++ bustibleLibs/src/main/java/com/bustiblelemons/logging/Logger.java
@@ -8,82 +8,88 @@ import java.util.Locale;
  * Created by bhm on 18.07.14.
  */
 public class Logger {
-    private static String mTag;
+    private String  mTag;
+    private boolean mForceLog;
 
     public Logger(Class<?> cls) {
+        this(cls, false);
+    }
+
+    public Logger(Class<?> cls, boolean forceLog) {
+        mForceLog = forceLog;
         this.mTag = cls.getSimpleName();
     }
 
-    public static int d(Throwable throwable) {
+    public int d(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int e(Throwable throwable) {
+    public int e(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int i(Throwable throwable) {
+    public int i(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int v(Throwable throwable) {
+    public int v(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int w(Throwable throwable) {
+    public int w(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int wtf(Throwable throwable) {
+    public int wtf(Throwable throwable) {
         return Log.d(mTag, throwable != null ? throwable.getLocalizedMessage() : "", throwable);
     }
 
-    public static int d(Throwable throwable, String format, Object... args) {
+    public int d(Throwable throwable, String format, Object... args) {
         return Log.d(mTag, String.format(Locale.ENGLISH, format, args), throwable);
     }
 
-    public static int e(Throwable throwable, String format, Object... args) {
+    public int e(Throwable throwable, String format, Object... args) {
         return Log.e(mTag, String.format(Locale.ENGLISH, format), throwable);
     }
 
-    public static int i(Throwable throwable, String format, Object... args) {
+    public int i(Throwable throwable, String format, Object... args) {
         return Log.i(mTag, String.format(Locale.ENGLISH, format, args), throwable);
     }
 
-    public static int v(Throwable throwable, String format, Object... args) {
+    public int v(Throwable throwable, String format, Object... args) {
         return Log.v(mTag, String.format(Locale.ENGLISH, format, args), throwable);
     }
 
-    public static int wtf(Throwable throwable, String format, Object... args) {
+    public int wtf(Throwable throwable, String format, Object... args) {
         return Log.wtf(mTag, String.format(Locale.ENGLISH, format, args), throwable);
     }
 
-    public static int w(Throwable throwable, String format, Object... args) {
+    public int w(Throwable throwable, String format, Object... args) {
         return Log.w(mTag, String.format(Locale.ENGLISH, format, args), throwable);
     }
 
 
-    public static int d(String format, Object... args) {
+    public int d(String format, Object... args) {
         return Log.d(mTag, String.format(Locale.ENGLISH, format, args));
     }
 
-    public static int e(String format, Object... args) {
+    public int e(String format, Object... args) {
         return Log.e(mTag, String.format(Locale.ENGLISH, format, args));
     }
 
-    public static int i(String format, Object... args) {
+    public int i(String format, Object... args) {
         return Log.i(mTag, String.format(Locale.ENGLISH, format, args));
     }
 
-    public static int v(String format, Object... args) {
+    public int v(String format, Object... args) {
         return Log.v(mTag, String.format(Locale.ENGLISH, format, args));
     }
 
-    public static int wtf(String format, Object... args) {
+    public int wtf(String format, Object... args) {
         return Log.wtf(mTag, String.format(Locale.ENGLISH, format, args));
     }
 
-    public static int w(String format, Object... args) {
+    public int w(String format, Object... args) {
         return Log.w(mTag, String.format(Locale.ENGLISH, format, args));
     }
 }
